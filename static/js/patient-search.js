ed9dc350fc7f7580e1573a1ccec23cf3723eacca
Emily Cheera <emilycheera@gmail.com>
2020-03-12 08:33:19 -0700

    Added sidebar links to nav on mobile view.

--- static/js/patient-search.js
+++ static/js/patient-search.js
@@ -9,3 +9,15 @@ $(document).ready(function() {
     });
 });
 
+
+$(document).ready(function() {
+    $(".patient-search").select2({
+        placeholder: "Patient Search",
+        allowClear: true,
+        theme: "bootstrap"
+    });
+    $("#patient-mobile").on("select2:select", function (e) {
+        window.location.replace(e.params.data.id);
+    });
+});
+
