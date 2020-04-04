df4167d7302453fc3bb567bbedd7428421233e3d
Emily Cheera <emilycheera@gmail.com>
2020-04-04 13:06:34 -0700

    Fixed bug in displaying unauthorized template.

--- decorators.py
+++ decorators.py
@@ -3,7 +3,8 @@ from functools import wraps
 from flask import render_template, session, request, redirect
 
 from users import (check_dietitian_authorization, check_patient_authorization,
-                   get_user_type_from_session, get_dietitian_and_patients_list)
+                   get_user_type_from_session, get_dietitian_and_patients_list,
+                   get_current_dietitian, get_current_patient)
 from model import Patient
 
 
@@ -11,9 +12,16 @@ def dietitian_auth(fn):
     """Check if a logged in dietitian is authorized to view the current page."""
     @wraps(fn)
     def decorated_view(*args, **kwargs):
+        user_type = get_user_type_from_session()
+        if user_type == "patient":
+            patient = get_current_patient()
+            return render_template("unauthorized.html", patient=patient)
+
         dietitian_id = kwargs["dietitian_id"]
         if not check_dietitian_authorization(dietitian_id):
-            return render_template("unauthorized.html")
+            dietitian = get_current_dietitian()
+            return render_template("unauthorized.html", dietitian=dietitian)
+                
         return fn(*args, **kwargs)
     return decorated_view
 
@@ -24,7 +32,8 @@ def dietitians_only(fn):
     def decorated_view(*args, **kwargs):
         user_type = get_user_type_from_session()
         if user_type != "dietitian":
-            return render_template("unauthorized.html")
+            patient = get_current_patient()
+            return render_template("unauthorized.html", patient=patient)
         return fn(*args, **kwargs)
     return decorated_view
 
@@ -39,13 +48,15 @@ def patient_or_dietitian_auth(fn):
         
         if user_type == "dietitian":
             patient = Patient.query.get(patient_id)
+            dietitian = get_current_dietitian()
             
             if not patient or (patient.dietitian_id != session.get("dietitian_id")):
-                return render_template("unauthorized.html")
+                return render_template("unauthorized.html", dietitian=dietitian)
 
         else:
             if not check_patient_authorization(patient_id):
-                return render_template("unauthorized.html")
+                patient = get_current_patient()
+                return render_template("unauthorized.html", patient=patient)
 
         return fn(*args, **kwargs)
     return decorated_view
@@ -56,11 +67,18 @@ def patient_belongs_to_dietitian(fn):
     @wraps(fn)
     def decorated_view(*args, **kwargs):
         
+        user_type = get_user_type_from_session()
+
+        if user_type == "patient":
+            patient = get_current_patient()
+            return render_template("unauthorized.html", patient=patient)
+
         patient_id = kwargs["patient_id"]
         patient = Patient.query.get(patient_id)
-            
+
         if not patient or (patient.dietitian_id != session.get("dietitian_id")):
-            return render_template("unauthorized.html")
+            dietitian = get_current_dietitian()
+            return render_template("unauthorized.html", dietitian=dietitian)
 
         return fn(*args, **kwargs)
     return decorated_view
@@ -70,9 +88,17 @@ def patient_auth(fn):
     """Check that the page is being visited by the logged in patient."""
     @wraps(fn)
     def decorated_view(*args, **kwargs):
+
+        user_type = get_user_type_from_session()
+        if user_type == "dietitian":
+            dietitian = get_current_dietitian()
+            return render_template("unauthorized.html", dietitian=dietitian)
+        
         patient_id = kwargs["patient_id"]
         if not check_patient_authorization(patient_id):
-            return render_template("unauthorized.html")
+            patient = get_current_patient()
+            return render_template("unauthorized.html", patient=patient)
+
         return fn(*args, **kwargs)
     return decorated_view
 
